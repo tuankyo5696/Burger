c579c5a10d2804e15429c013d8e1621ba640cdef
Tran Tuan <trananhtuan526201@gmail.com>
2019-05-01 23:31:01 +0700

    second commit

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -2,6 +2,7 @@ import React,{Component} from 'react';
 
 import Layout from './components/Layout/Layout' ;
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+
 import './App.css';
 
 class App extends Component {
--- myapp/src/components/Burger/Burger.js
+++ myapp/src/components/Burger/Burger.js
@@ -4,12 +4,18 @@ import classes from './Burger.css';
 import BurgerIngredient from './Burgeringredient/Burgeringredient';
 
 const burger = (props) => {
-    const transformedIngredients = Object.keys(props.ingredients).map(
+    let transformedIngredients = Object.keys(props.ingredients).map(
         igKey =>{
             return [...Array(props.ingredients[igKey])].map((_,i)=>{
-              return  <BurgerIngredient key={igKey + 1} type = {igKey} />
+              return  <BurgerIngredient key={igKey+ i} type = {igKey} />
             });
-        });
+        })
+        .reduce((arr,el)=>{
+            return arr.concat(el); // simply flattern
+        },[])
+        if(transformedIngredients.length === 0){
+            transformedIngredients = <p>Please start adding ingredients!</p>
+        }
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type ="bread-top"/>
